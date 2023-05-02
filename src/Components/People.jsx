f3968157fd0e0a93bbfef6c02fc3f22cc04e90e2
Sarah Whalley <s.whalley1130@gmail.com>
2023-05-02 15:35:43 -0600

    feat(): formatted and fixed user info grid

--- src/Components/People.jsx
+++ src/Components/People.jsx
@@ -1,47 +1,50 @@
-import React, {useState, useEffect, useContext} from "react";
+import React, { useState, useEffect, useContext } from "react";
 import Nav from './Nav'
 import { useNavigate } from "react-router";
-import { UserContext} from "../user_context";
+import { UserContext } from "../user_context";
 
-function People(){
+function People() {
 
-    const nav=useNavigate();
-    const {user, setUser}=useContext(UserContext);
-    const [allUsers, setAllUsers]=useState([])
-    const [isLoaded, setIsLoaded]=useState(false)
-    const [friends, setFriends]=useState([])
+    const nav = useNavigate();
+    const { user, setUser } = useContext(UserContext);
+    const [allUsers, setAllUsers] = useState([])
+    const [isLoaded, setIsLoaded] = useState(false)
+    const [friends, setFriends] = useState([])
 
-    if(!user){
-        useEffect(()=>{
+    if (!user) {
+        useEffect(() => {
             return nav('/');
-        },[])
+        }, [])
     }
 
-    useEffect(()=>
-    {
+    useEffect(() => {
         fetch(`/api/users`)
-        .then(r=>r.json())
-        .then(data=>setAllUsers(data))
+            .then(r => r.json())
+            .then(data => setAllUsers(data))
 
         fetch(`/api/friends`)
-        .then(r=>r.json())
-        .then(data=>setFriends(data))
+            .then(r => r.json())
+            .then(data => setFriends(data))
 
         setIsLoaded(true)
-    },[])
+    }, [])
 
+    console.log('allUsers', allUsers)
 
     return (
         <>
-            <Nav/>
-            {isLoaded ? 
-                <div style={{width:'80%', maxWidth:'80rem',margin:'auto', padding:'28px'}}>
-                    Loaded!
+            <Nav />
+            {isLoaded
+                ? <div style={{ width: '80%', maxWidth: '80rem', margin: 'auto', padding: '28px' }}>
+                    {allUsers.map(u => 
+                        <div style={{display: 'flex', justifyContent: 'space-between', alignContent: 'start'}}>
+                            <p>{u.username}</p>
+                            <p>{u.email}</p>
+                            <p>{u.id}</p>
+                        </div>
+                    )}
                 </div>
-            :
-                <>
-                    Loading...
-                </>
+                : <>Loading...</>
             }
         </>
     )
